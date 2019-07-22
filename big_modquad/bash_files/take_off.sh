7e9798431265525152da11d3ebafd23e0ad95f1c
yehonathan litman <yehonathan.litman@stonybrook.edu>
2019-07-22 05:57:23 -0400

    tiny fix for offboard change

--- big_modquad/bash_files/take_off.sh
+++ big_modquad/bash_files/take_off.sh
@@ -4,7 +4,7 @@ rosservice call /modquad0/send_waypoint "x: 0.0
 y: 0.0
 z: 1.0
 yaw: 0.0"
-echo "Modquad1 take off"
+echo "Modquad0 take off"
 read -n 1 -s
 
 rosservice call /modquad1/send_waypoint "x: 2.0
