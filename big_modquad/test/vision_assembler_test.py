082817d87fd3126c2a3e358fb75a86a1270600d6
yehonathan litman <yehonathan.litman@stonybrook.edu>
2020-02-03 08:18:45 -0500

    simple docking animation

--- big_modquad/test/vision_assembler_test.py
+++ big_modquad/test/vision_assembler_test.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-from matplotlib import rc
+from matplotlib import rc, animation
 import numpy as np
 import collections
 
@@ -39,7 +39,7 @@ def constrained_bfs(G, source):
 			queue.popleft()
 
 def assembly_tree(G, master):
-	#T = nx.bfs_tree(G, master)
+	#return nx.bfs_tree(G, master)
 	T = nx.DiGraph()
 	T.add_node(master)
 	edges = []
@@ -51,13 +51,13 @@ def assembly_tree(G, master):
 def sequence(T, step):
 	if len(T) > 1:
 		leaves = [i for i in T.nodes if T.out_degree(i) == 0 
-						and T.in_degree(i) == 1]
+						and T.in_degree(i) <= 1]
 		D = []
 		master = [i for i in T.nodes if T.in_degree(i) == 0][0]
 		leaves_size = len(leaves) + 1 if T.out_degree(master) == 1 else len(leaves)
 		if leaves_size == 0: #temporary hack
 			for l in T.nodes:
-				D.append((l, step + 1))
+				D.append((l, step))
 			return D
 
 		limit = int(np.floor(len(T)/leaves_size))
@@ -113,6 +113,47 @@ def flatten_remove(D, M):
 def sort_by_step(D):
 	return sorted(D, key = lambda dis: dis[1], reverse = True)
 
+def move(D, T, pos, dt): #inefficient but whatever
+	if len(D) == 0:
+		return True
+	high = D[0][1] #max step
+	candidates = [(k,v) for k, v in D if v == high]
+	for k, v in candidates:
+		p = [i for i in T.pred[k]][0]
+		diff = tuple(xi - xd for xi, xd in zip(pos[k], pos[p]))
+		pos[k] = (pos[k][0] - diff[0]*dt, pos[k][1] - diff[1]*dt)
+		if abs(diff[0]) < 0.21 and abs(diff[1]) < 0.115:
+			return True
+	return False
+
+def update(i, T, D, M, pos, color_map, ax, dt):
+	ax.clear()
+
+	done = move(D, T, pos, dt)
+	if done is True:
+		try:
+			current = D.pop(0)[1]
+			while D[0][1] == current:
+				D.pop(0)
+		except IndexError:
+			plt.close()
+		
+	nx.draw_networkx(T_plt, pos, with_labels=False, arrows=True, node_color=color_map, ax=ax, node_size=1000, node_shape="s")
+
+	ax.set_title("Frame {}".format(i))
+
+def animate(T, D, M, pos, color_map):
+	fig, ax = plt.subplots(figsize=(7,7))
+	dt = 1./30
+
+	anim = animation.FuncAnimation(fig, update, interval=20, fargs=(T, D, M, pos, color_map, ax, dt), save_count=300)
+
+	try:
+		plt.show()
+		#anim.save('animation.mp4', fps=30, extra_args=["-vcodec","libx264"])
+	except AttributeError:
+		pass
+
 def color_node(n):
 	mapping = {
 		1: 'coral',
@@ -123,13 +164,12 @@ def color_node(n):
 		6: 'magenta',
 		7: 'cyan',
 		8: 'olive',
-		9: 'yellow',
-		10: 'red' #1024 modules 
+		9: 'yellow' 
 	}
 	return mapping.get(n, 'lightgray')
 
 if __name__ == "__main__":
-	'''
+	#'''
 	#Lattice
 	G = nx.Graph()
 	G.add_nodes_from(list(range(9)))
@@ -142,8 +182,8 @@ if __name__ == "__main__":
 	5: (1.0, 2.0),
 	6: (2.0, 0.0),
 	7: (2.0, 1.0),
-	8: (2.0, 2.0),
-	9: (1.0, 3.0)
+	8: (2.0, 2.0)
+	#9: (1.0, 3.0)
 	}
 	G.add_edge(0,1)
 	G.add_edge(1,2)
@@ -158,7 +198,7 @@ if __name__ == "__main__":
 	G.add_edge(4,1)
 	G.add_edge(5,2)
 	#G.add_edge(5,9)
-	'''
+	#'''
 	'''
 	#Loop
 	G = nx.path_graph(24)
@@ -190,7 +230,7 @@ if __name__ == "__main__":
         }
 	G.add_edge(23,0)
 	'''
-	#'''
+	'''
 	#Bridges
         G = nx.Graph()
         G.add_nodes_from(list(range(40)))
@@ -221,19 +261,24 @@ if __name__ == "__main__":
         23: (-8.0, 3.0),
         24: (-3.0, 4.0),
         25: (-4.0, 4.0),
-        26: (-1.0, 5.0),
+        32: (-1.0, 5.0),
         27: (-2.0, 5.0),
         28: (-3.0, 5.0),
         29: (-4.0, 5.0),
         30: (0.0, 6.0),
         31: (-1.0, 6.0),
-        32: (-2.0, 6.0),
+        26: (-2.0, 6.0),
         33: (0.0, 7.0),
         34: (-1.0, 7.0),
-        35: (0.0, 8.0),
+        #35: (0.0, 8.0),
+        #36: (-1.0, 8.0),
+        #37: (-2.0, 8.0),
+        #38: (-1.0, 9.0),
+        #39: (-2.0, 9.0),
+        38: (0.0, 8.0),
         36: (-1.0, 8.0),
         37: (-2.0, 8.0),
-        38: (-1.0, 9.0),
+        35: (-1.0, 9.0),
         39: (-2.0, 9.0),
         }
         G.add_edge(0,3)
@@ -276,22 +321,22 @@ if __name__ == "__main__":
         G.add_edge(24,25)
         G.add_edge(29,28)
         G.add_edge(28,27)
-        G.add_edge(26,27)
-        G.add_edge(26,31)
-        G.add_edge(27,32)
+        G.add_edge(32,27)
+        G.add_edge(32,31)
+        G.add_edge(27,26)
         G.add_edge(30,31)
-        G.add_edge(31,32)
+        G.add_edge(31,26)
         G.add_edge(30,33)
         G.add_edge(31,34)
         G.add_edge(33,34)
-        G.add_edge(33,35)
+        G.add_edge(33,38)
         G.add_edge(34,36)
-        G.add_edge(35,36)
-        G.add_edge(37,36)
         G.add_edge(38,36)
+        G.add_edge(37,36)
+        G.add_edge(35,36)
         G.add_edge(39,37)
-        G.add_edge(39,38)
-	#'''
+        G.add_edge(39,35)
+	'''
 	'''
 	#Hole-in-middle
         G = nx.Graph()
@@ -320,7 +365,7 @@ if __name__ == "__main__":
         20: (-5.0, 4.0),
         21: (-4.0, 5.0),
         22: (-5.0, 5.0),
-        23: (2.0, 5.0),
+        33: (2.0, 5.0),
         24: (3.0, 5.0),
         25: (-4.0, 6.0),
         26: (-5.0, 6.0),
@@ -330,7 +375,7 @@ if __name__ == "__main__":
         30: (0.0, 6.0),
         31: (1.0, 6.0),
         32: (2.0, 6.0),
-        33: (3.0, 6.0),
+        23: (3.0, 6.0),
         34: (0.0, 7.0),
         35: (-1.0, 7.0),
         36: (-2.0, 7.0),
@@ -368,13 +413,13 @@ if __name__ == "__main__":
         G.add_edge(19,21)
         G.add_edge(22,20)
         G.add_edge(12,24)
-        G.add_edge(13,23)
+        G.add_edge(13,33)
         G.add_edge(21,25)
         G.add_edge(22,26)
         G.add_edge(21,22)
-        G.add_edge(23,24)
-        G.add_edge(23,32)
-        G.add_edge(24,33)
+        G.add_edge(33,24)
+        G.add_edge(33,32)
+        G.add_edge(24,23)
         G.add_edge(25,26)
         G.add_edge(25,27)
         G.add_edge(28,27)
@@ -382,7 +427,7 @@ if __name__ == "__main__":
         G.add_edge(30,29)
         G.add_edge(30,31)
         G.add_edge(32,31)
-        G.add_edge(32,33)
+        G.add_edge(32,23)
         G.add_edge(30,34)
         G.add_edge(35,34)
         G.add_edge(35,36)
@@ -398,7 +443,7 @@ if __name__ == "__main__":
         G.add_edge(37,27)
 	'''
 
-	M = select_master() #for Hole-in-middle scenario, choose master 24
+	M = select_master() #for Hole-in-middle scenario, choose master 12
 	print("Master: {}".format(M))
 	T = assembly_tree(G, M)
 	T_plt = T.copy()
@@ -410,9 +455,13 @@ if __name__ == "__main__":
 	color_map = [0] * len(G)
 	color_map[M] = 'lightgray'
 	for step in D:
-		labels[step[0]] = r"${}$".format(step[1])
-		color_map[step[0]] = color_node(step[1])
-	nx.draw_networkx(T_plt, pos, labels=labels, arrows=True, node_color=color_map, node_size=1000, node_shape="s")
-	plt.xlabel(r"$x$", fontsize = 14)
-	plt.ylabel(r"$y$", fontsize = 14)
-	plt.show()
+		p = [i for i in T_plt.pred[step[0]]][0]
+		if p == M:
+			color_map[step[0]] = "red"
+		else:
+			labels[step[0]] = r"${}$".format(step[1])
+			color_map[step[0]] = color_node(step[1])
+	animate(T_plt, D, M, pos, color_map)
+	#plt.xlabel(r"$x$", fontsize = 14)
+	#plt.ylabel(r"$y$", fontsize = 14)
+	#nx.draw_networkx(T_plt, pos, labels=labels, arrows=True, node_color=color_map, ax=ax, node_size=1000, node_shape="s")
